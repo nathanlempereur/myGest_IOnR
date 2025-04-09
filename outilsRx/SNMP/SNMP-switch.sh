2c37ed99246eded6270323d9e6ea0f1e7a7b16f1
nathan lempereur <167067383+nathanlempereur@users.noreply.github.com>
2025-04-09 14:40:18 +0200

    Update SNMP-switch.sh
    Port WAN

--- outilsRx/SNMP/SNMP-switch.sh
+++ outilsRx/SNMP/SNMP-switch.sh
@@ -9,64 +9,78 @@ while ! validIP "$IP"; do
     echo "Veuillez saisir une adresse IP :"
     read IP
 done
+
+echo "Veuillez saisir le port WAN :"
+read WAN
 echo "Envoie de la requête SNMP ...
 
 *********************************
          MAC	| PORT | STATUS *
 *********************************"
 
-
 if ! command -v snmpwalk &> /dev/null; then
     echo "Erreur : snmpwalk n'est pas installé. Installez-le avant d'exécuter ce script."
     exit 1
 fi
 
+# Extraction des adresses MAC (en excluant le port WAN et le port 0)
 snmpwalk -v2c -c public "$IP" 1.3.6.1.2.1.17.4.3.1.2 | \
-    awk -F': ' '$2 > 1 {print $1}' | \
+    awk -F': ' -v wan="$WAN" '
+        {
+            gsub(/ /,"",$2);
+            if ($2 != wan && $2 != "0") print $1
+        }' | \
     awk -F. '{for(i=NF-5;i<=NF;i++) printf "%02X%s", $i, (i==NF ? "\n" : ":")}' > MAC.txt
 
-# Extraction des numéros de ports associés aux adresses MAC et stockage dans PORT.txt
+# Extraction des ports associés (toujours en excluant le port WAN et le port 0)
 snmpwalk -v2c -c public "$IP" 1.3.6.1.2.1.17.4.3.1.2 | \
-    awk -F': ' '$2 > 1 {split($1, a, "."); mac=""; for(i=NF-5;i<=NF;i++) mac=sprintf("%s%d%s", mac, a[i], (i==NF ? "" : ".")); print mac, $2}' | \
-    awk '{print $NF}' > PORT.txt
+    awk -F': ' -v wan="$WAN" '
+        {
+            gsub(/ /,"",$2);
+            if ($2 != wan && $2 != "0") {
+                split($1, a, ".");
+                mac="";
+                for(i=NF-5;i<=NF;i++) mac=sprintf("%s%d%s", mac, a[i], (i==NF ? "" : "."));
+                print mac, $2
+            }
+        }' | awk '{print $NF}' > PORT.txt
 
-# Récupération des statuts des ports et stockage dans UP.txt
+# Récupération des statuts (1 = UP, 2 = DOWN)
 snmpwalk -v2c -c public "$IP" 1.3.6.1.2.1.2.2.1.8 | \
-    awk -F'.' '/iso.3.6.1.2.1.2.2.1.8./ {split($0, a, "="); print a[1], a[2]}' | \
-    awk '{print $NF}' > UP.txt
+    awk -F'.' '/iso.3.6.1.2.1.2.2.1.8./ {
+        split($0, a, "=");
+        print a[1], a[2]
+    }' | awk '{print $NF}' > UP.txt
 
-# Chargement des ports et de leurs statuts dans des tableaux
+# Lecture des ports et statuts dans des tableaux
 mapfile -t PORTS < PORT.txt
 mapfile -t UP_STATUS < UP.txt
 
-# Nombre total de ports trouvés
 port_count=${#PORTS[@]}
-
-# Initialisation du compteur pour les ports
 index=0
 
-# Associer les adresses MAC aux ports et leur état UP/DOWN
+# Affichage des résultats
 while read -r MAC; do
     if [ $index -lt $port_count ]; then
         PORT=${PORTS[index]}
         STATUS=${UP_STATUS[index]}
         
-        # Conversion de l'état numérique en texte lisible
         case $STATUS in
             1) STATUS="UP" ;;
             2) STATUS="DOWN" ;;
             *) STATUS="UNKNOWN" ;;
         esac
-        
-        echo "$MAC | $PORT | $STATUS
-****************************"
+
+        echo "$MAC | $PORT | $STATUS"
+        echo "****************************"
         ((index++))
     else
-        echo "$MAC No available port"
+        echo "$MAC | No available port"
     fi
 done < MAC.txt
 
-rm MAC.txt PORT.txt UP.txt
+# Nettoyage
+rm -f MAC.txt PORT.txt UP.txt
 
 echo "
 [Appuyer sur entrer pour continuer]"
